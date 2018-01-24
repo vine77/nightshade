f8dc7f2873671a2b633a1d51d94485a816a0fc3c
Nathan Ward <ward@digivine.com>
2018-01-24 10:37:24 -0800

    Create persisted list

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    try {
+      this.state = { tasks: JSON.parse(localStorage.getItem('tasks')) || [] }
+    } catch (error) {
+      this.state = { tasks: [] }
+    }
+
+    this.updateTaskName = this.updateTaskName.bind(this)
+    this.addTask = this.addTask.bind(this)
+  }
+
+  addTask() {
+    this.setState({ tasks: [...this.state.tasks, { title: '' }] })
+  }
+
+  updateTaskName(index, event) {
+    this.setState({
+      tasks: [
+        ...this.state.tasks.slice(0, index),
+        { ...this.state.tasks[index], title: event.target.value },
+        ...this.state.tasks.slice(index + 1),
+      ],
+    })
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div>
+        <header>
+          <h1>Nightshade</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul className="tasks">
+          {this.state.tasks.map((task, index) => (
+            <li key={index}>
+              <input
+                value={task.title}
+                onChange={this.updateTaskName.bind(this, index)}
+              />
+            </li>
+          ))}
+          <li>
+            <button onClick={this.addTask}>Add task</button>
+          </li>
+        </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
